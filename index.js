41b2e37442f33c289d7fe9988b00b8355fad6157
Seldo <github@seldo.com>
2013-08-20 20:01:31 -0700

    Comments

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 module.exports = Adapter;
 function Adapter() {}
-Adapter.prototype.connect = function() {
+
+/**
+ * Called at application startup.
+ * @param connectionParams The configuration parameters
+ * - If your datasource supports connection pooling, now's the time to do it
+ * - If your datasource needs to poll in the background, start doing that and then call back
+ * -
+ */
+Adapter.prototype.connect = function(connectionParams,cb) {
   throw new Error('not implemented');
 }
 Adapter.prototype.validate = function(rule,params) {
